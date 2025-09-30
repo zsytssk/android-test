6328a7eeb0532262c3d53d1078d2d3801647f174
zsytssk <zsytssk@gmail.com>
2025-09-30 08:35:01 +0800

    [fix] [master]

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,42 +1,43 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
+            SecondScreen(onBack = { finish() })
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    val context = LocalContext.current
+fun SecondScreen(onBack: () -> Unit) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Button(onClick = {
-            val intent = Intent(context, SecondActivity::class.java)
-            context.startActivity(intent)
-        }) {
-            Text("Open new activity")
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Second screen!")
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(onClick = onBack) {
+                Text(text = "Back")
+            }
         }
     }
 }
